8959c28616e1806abfcc78b0cf556c75398ab84d
DARIYA.THORN <DARIYA.THORN@PNC.LAN>
2023-01-26 08:01:10 +0700

    [PDN-39] display show on homepage

--- views/home/index.view.php
+++ views/home/index.view.php
@@ -2,7 +2,6 @@
 <?php require "views/partials/nav.php" ?>
 <?php require "views/partials/banner.php" ?>
 
-
 <main class="h-screen">
   <div class="p-3">
     <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
