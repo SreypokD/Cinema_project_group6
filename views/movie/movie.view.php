b6edacc8f9f20177470b7818d9ca30a0b25fa4d1
ileanan <dynahen133@gmail.com>
2023-01-26 15:49:58 +0700

    create card list of movie

--- views/movie/movie.view.php
+++ views/movie/movie.view.php
@@ -2,10 +2,148 @@
 <?php require "views/partials/nav.php" ?>
 <?php require "views/partials/banner.php" ?>
 
-<main class="h-screen">
+<!-- <main class="h-screen">
   <div class="p-3">
     <p>Hello movie page</p>
   </div>
-</main>
+</main> -->
+
+<div class="flex justify-evenly mb-10 grid grid-cols-4 gap-4">
+
+        <div class="bg-red-500 p-3 flex flex-col justify-center mt-10" id="card">  
+          <div class=" max-w-sm flex justify-center items-center content-div rounded-md">
+              <img src="../../asset/image/mm.jpg" alt="" class="ml-8 rounded-md" id="img">       
+                  <div></div>
+              <div>
+                  <div  class="w-full image-cover rounded-t-md" ></div>
+                  <div class="py-8 px-4 rounded-b-md fd-cl group-hover:opacity-25">
+                      <span class="block text-gray-400 text-sm">
+                      </span>
+                  </div>
+              </div>
+              <div class="absolute opacity-0 fd-sh group-hover:opacity-100 group-hover:buttom-0">
+                  <span class="text-2xl text-white tracking-wider leading-relaxed font-sans">Movie time</span> 
+                  <div class="pt-8 text-center flex flex-col items-center ">
+                 <button class="text-white p-2 bg-red-600  text-white font-bold text-sm w-30 rounded-md"><a href="https://www.w3schools.com/">SEE MORE</a></button>
+                 <button class="text-white p-2 bg-red-600  text-white font-bold text-sm w-30 mt-5 rounded-md"><a href="https://majorcineplex.com.kh/home">BUY NOW</a></button>
+             </div>
+        </div>
+          </div>
+  
+              <div class="flex justify-end">
+                  <p class="text-white text-xl mb-5 font-medium" id="p">hello</p>
+              </div>
+          </div>
+
+
+
+          <div class="bg-red-500 p-3 flex flex-col justify-center mt-10" id="card">  
+          <div class=" max-w-sm flex justify-center items-center content-div rounded-md">
+              <img src="../../asset/image/mm.jpg" alt="" class="ml-8 rounded-md" id="img">       
+                  <div></div>
+              <div>
+                  <div  class="w-full image-cover rounded-t-md" ></div>
+                  <div class="py-8 px-4 rounded-b-md fd-cl group-hover:opacity-25">
+                      <span class="block text-gray-400 text-sm">
+                      </span>
+                  </div>
+              </div>
+              <div class="absolute opacity-0 fd-sh group-hover:opacity-100 group-hover:buttom-0">
+                  <span class="text-2xl text-white tracking-wider leading-relaxed font-sans">Movie time</span> 
+                  <div class="pt-8 text-center flex flex-col items-center ">
+                 <button class="text-white p-2 bg-red-600  text-white font-bold text-sm w-30 rounded-md"><a href="https://www.w3schools.com/">SEE MORE</a></button>
+                 <button class="text-white p-2 bg-red-600  text-white font-bold text-sm w-30 mt-5 rounded-md"><a href="https://majorcineplex.com.kh/home">BUY NOW</a></button>
+             </div>
+        </div>
+          </div>
+  
+              <div class="flex justify-end">
+                  <p class="text-white text-xl mb-5 font-medium" id="p">hello</p>
+              </div>
+          </div>
+
+
+
+
+          <div class="bg-red-500 p-3 flex flex-col justify-center mt-10" id="card">  
+          <div class=" max-w-sm flex justify-center items-center content-div rounded-md">
+              <img src="../../asset/image/mm.jpg" alt="" class="ml-8 rounded-md" id="img">       
+                  <div></div>
+              <div>
+                  <div  class="w-full image-cover rounded-t-md" ></div>
+                  <div class="py-8 px-4 rounded-b-md fd-cl group-hover:opacity-25">
+                      <span class="block text-gray-400 text-sm">
+                      </span>
+                  </div>
+              </div>
+              <div class="absolute opacity-0 fd-sh group-hover:opacity-100 group-hover:buttom-0">
+                  <span class="text-2xl text-white tracking-wider leading-relaxed font-sans">Movie time</span> 
+                  <div class="pt-8 text-center flex flex-col items-center ">
+                 <button class="text-white p-2 bg-red-600  text-white font-bold text-sm w-30 rounded-md"><a href="https://www.w3schools.com/">SEE MORE</a></button>
+                 <button class="text-white p-2 bg-red-600  text-white font-bold text-sm w-30 mt-5 rounded-md"><a href="https://majorcineplex.com.kh/home">BUY NOW</a></button>
+             </div>
+        </div>
+          </div>
+  
+              <div class="flex justify-end">
+                  <p class="text-white text-xl mb-5 font-medium" id="p">hello</p>
+              </div>
+          </div>
+
+
+
+          <div class="bg-red-500 p-3 flex flex-col justify-center mt-10" id="card">  
+          <div class=" max-w-sm flex justify-center items-center content-div rounded-md">
+              <img src="../../asset/image/mm.jpg" alt="" class="ml-8 rounded-md" id="img">       
+                  <div></div>
+              <div>
+                  <div  class="w-full image-cover rounded-t-md" ></div>
+                  <div class="py-8 px-4 rounded-b-md fd-cl group-hover:opacity-25">
+                      <span class="block text-gray-400 text-sm">
+                      </span>
+                  </div>
+              </div>
+              <div class="absolute opacity-0 fd-sh group-hover:opacity-100 group-hover:buttom-0">
+                  <span class="text-2xl text-white tracking-wider leading-relaxed font-sans">Movie time</span> 
+                  <div class="pt-8 text-center flex flex-col items-center ">
+                 <button class="text-white p-2 bg-red-600  text-white font-bold text-sm w-30 rounded-md"><a href="https://www.w3schools.com/">SEE MORE</a></button>
+                 <button class="text-white p-2 bg-red-600  text-white font-bold text-sm w-30 mt-5 rounded-md"><a href="https://majorcineplex.com.kh/home">BUY NOW</a></button>
+             </div>
+        </div>
+          </div>
+  
+              <div class="flex justify-end">
+                  <p class="text-white text-xl mb-5 font-medium" id="p">hello</p>
+              </div>
+          </div>
+
+
+
+          <div class="bg-red-500 p-3 flex flex-col justify-center mt-10" id="card">  
+          <div class=" max-w-sm flex justify-center items-center content-div rounded-md">
+              <img src="../../asset/image/mm.jpg" alt="" class="ml-8 rounded-md" id="img">       
+                  <div></div>
+              <div>
+                  <div  class="w-full image-cover rounded-t-md" ></div>
+                  <div class="py-8 px-4 rounded-b-md fd-cl group-hover:opacity-25">
+                      <span class="block text-gray-400 text-sm">
+                      </span>
+                  </div>
+              </div>
+              <div class="absolute opacity-0 fd-sh group-hover:opacity-100 group-hover:buttom-0">
+                  <span class="text-2xl text-white tracking-wider leading-relaxed font-sans">Movie time</span> 
+                  <div class="pt-8 text-center flex flex-col items-center ">
+                 <button class="text-white p-2 bg-red-600  text-white font-bold text-sm w-30 rounded-md"><a href="https://www.w3schools.com/">SEE MORE</a></button>
+                 <button class="text-white p-2 bg-red-600  text-white font-bold text-sm w-30 mt-5 rounded-md"><a href="https://majorcineplex.com.kh/home">BUY NOW</a></button>
+             </div>
+        </div>
+          </div>
+  
+              <div class="flex justify-end">
+                  <p class="text-white text-xl mb-5 font-medium" id="p">hello</p>
+              </div>
+          </div>
+
+</div>
 
   <?php require "views/partials/footer.php" ?>
