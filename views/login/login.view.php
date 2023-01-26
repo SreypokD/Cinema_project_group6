fcdce7c43db885ca289556a7ba2f5182def63653
Phearom Chea <phearomchea210920@gmail.com>
2023-01-26 13:29:54 +0700

    [PDN-30] create views form login

--- views/login/login.view.php
+++ views/login/login.view.php
@@ -2,10 +2,77 @@
 <?php require "views/partials/nav.php" ?>
 <?php require "views/partials/banner.php" ?>
 
-<main class="h-screen">
-  <div class="p-3">
-    <p>Hello Login</p>
+<form action="#" class="mx-96 my-10 bg-black p-8 rounded-xl">
+  <h1 class="text-xl mb-2 text-center font-bold leading-tight tracking-tight text-white md:text-2xl dark:text-white">
+    SING IN YOUR ACCOUNT
+  </h1>
+  <div class="bg-red-100 border-t-4 border-red-500 rounded-b text-red-900 px-4 py-3 shadow-md mb-5" role="alert">
+    <div class="flex">
+      <div class="py-1"><svg class="fill-current h-6 w-6 text-red-500 mr-4" xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20">
+          <path
+            d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+        </svg></div>
+      <div>
+        <p class="font-bold">Our privacy policy has changed</p>
+        <p class="text-sm">Make sure you know how these changes affect you.</p>
+      </div>
+    </div>
   </div>
-</main>
+  <div class="relative z-0 w-full mb-6 group">
+    <input type="email" name="floating_email" id="floating_email"
+      class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-white peer"
+      placeholder=" " />
+    <label for="floating_email"
+      class="pl-6 peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email
+      address</label>
+    <svg
+      class="w-5 h-5 dark:text-white peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+        d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75">
+      </path>
+    </svg>
+  </div>
+  <div class="relative z-0 w-full mb-6 group">
+    <input type="password" name="floating_password" id="floating_password"
+      class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-white focus:outline-none focus:ring-0 focus:border-white peer"
+      placeholder=" " />
+    <label for="floating_password"
+      class="pl-6 peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+    <svg
+      class="w-5 h-5 dark:text-white peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+        d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z">
+      </path>
+    </svg>
+  </div>
+  <div class="flex items-center justify-between my-4">
+    <div class="flex items-start">
+      <div class="flex items-center h-5">
+        <input id="remember" aria-describedby="remember" type="checkbox"
+          class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800 checked:bg-red">
+      </div>
+      <div class="ml-3 text-sm">
+        <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+      </div>
+    </div>
+    <a href="#" class="text-sm font-medium text-red-600 hover:underline dark:text-red-500">Forgot
+      password?</a>
+  </div>
+  <div class="grid md:gap-6">
+    <div class="relative z-0 w-full mb-6 group">
+      <button
+        class="font-bold w-full text-white bg-red-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-600 dark:focus:ring-primary-800">
+        Sign IN
+      </button>
+    </div>
+  </div>
+  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+    Don’t have an account yet? <a href="/register"
+      class="font-medium text-red-600 hover:underline dark:text-red-500">Register</a>
+  </p>
+</form>
 
-  <?php require "views/partials/footer.php" ?>
+<?php require "views/partials/footer.php" ?>
