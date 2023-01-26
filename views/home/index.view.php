2a0b56d6ad0a59be1c74201677983c4715b5f203
DARIYA.THORN <DARIYA.THORN@PNC.LAN>
2023-01-26 07:55:23 +0700

    [PDN-39] display show on home page

--- views/home/index.view.php
+++ views/home/index.view.php
@@ -5,7 +5,53 @@
 
 <main class="h-screen">
   <div class="p-3">
-    <p>Hello Home page</p>
+    <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+      <!--Card 1-->
+      <div class="rounded overflow-hidden shadow-lg bg-red-800">
+          <div class="w-full">
+              <img class="w-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTexKqTptw8NkQKlVdmEFXlCmWsEf1K_Z7wCQ&usqp=CAU" alt="">
+          </div>
+        
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">Avatar part 3</div>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <span class="inline-block bg-gray-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Sunday</span>
+          <span class="inline-block bg-gray-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">12th 02 2023</span>
+          <span class="inline-block bg-gray-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Soon at Data Movie</span>
+        </div>
+      </div>
+      <!--Card 2-->
+      <div class="rounded overflow-hidden shadow-lg bg-red-800">
+        <div class="w-full">
+            <img class="w-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe6flptxCpRKQviOuOIunMg7ZGhDw7ovx80Urkm2TxZkYTb9O0OxaqrcUdapLmCaXN8xU&usqp=CAU" alt="">
+        <div class="px-6 py-4">
+        </div>
+        
+          <div class="font-bold text-xl mb-2">Avatar part 2</div>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <span class="inline-block bg-gray-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Friday</span>
+          <span class="inline-block bg-gray-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">24th 02 2023</span>
+          <span class="inline-block bg-gray-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Soon at steng meanchey</span>
+        </div>
+      </div>
+
+      <!--Card 3-->
+      <div class="rounded overflow-hidden shadow-lg bg-red-800">
+        <div class="w-full">
+            <img class="w-full" src="https://lumiere-a.akamaihd.net/v1/images/csr_yt_ae2ddc55.jpeg" alt="">
+        </div>
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">Avatar part 1</div>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <span class="inline-block bg-gray-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Thursday</span>
+          <span class="inline-block bg-gray-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">24th 01 2023</span>
+          <span class="inline-block bg-gray-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Soon Passerelles Movie</span>
+        </div>
+      </div>
+    </div>
   </div>
 </main>
   
