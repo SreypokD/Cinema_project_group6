f6dcb0e71db041f5792eb1ce302b29f2dd1bb177
Sreypok-D <sreypokdoem@gmail.com>
2023-02-09 15:39:20 +0700

    [PDN-71] display movie get from database in movie page

--- controllers/seller/seller.create.controller.php
+++ controllers/seller/seller.create.controller.php
@@ -1,18 +1,23 @@
 <?php
 require "database/database.php";
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $title = $_POST['title'];
-        $description = $_POST['description'];
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $title = $_POST['title'];
+    $description = $_POST['description'];
         $language = $_POST['language'];
         $releast_date = $_POST['releast_date'];
         $duration = $_POST['duration'];
         $picture = $_POST['picture'];
-        echo $title ;
-    // if (!empty($_POST['title']) && !empty($_POST['description']) && !empty($_POST['language']) && !empty($_POST['releast_date']) && !empty($_POST['duration']) && !empty($_POST['picture'])){
-    //     require("models/seller/seller.model.php");
-    //     CreateMovie($_POST['title'], $_POST['description'] , $_POST['language'], $_POST['releast_date'], $_POST['duration'], $_POST['picture']);
-     
-        
-    // }
-}
+        $start_date = $_POST['start_date'];
+        $end_date = $_POST['end_date'];
+        $format = $_POST['format'];
+        $subtitle = $_POST['subtitle'];
+        if (!empty($_POST['title']) && !empty($_POST['description']) && !empty($_POST['language']) && !empty($_POST['releast_date']) && !empty($_POST['duration']) && !empty($_POST['picture']) && !empty($_POST['start_date']) && !empty($_POST['end_date']) && !empty($_POST['format'])&& !empty($_POST['subtitle'])){
+            require("models/seller/seller.model.php");
+            CreateMovie($title , $description , $language , $releast_date, $duration ,$picture, $start_date, $end_date , $format , $subtitle);
+            // echo $title ."<br>",$description."<br>" ,$language ."<br>"  , $releast_date ."<br>", $duration."<br>" ,$picture ."<br>" , $date_start ."<br>", $date_end."<br>" , $format."<br>" ,$sub_title;
+            // header("Location: /seller");
+            
+            }
+        }
--- models/seller/seller.model.php
+++ models/seller/seller.model.php
@@ -1,9 +1,9 @@
 <?php
 
-    function CreateMovie(string $title, string $description, string $language , string $releast_date , string $duration , string $picture) : bool
+    function CreateMovie(string $title, string $description, string $language , string $releast_date , string $duration , string $picture ) : bool
     {
         global $connection;
-        $statement = $connection->prepare("INSERT INTO movies(title, description,language, releast_date, duration,picture) values (:title, :description,:language,:releast_date,:duration,:picture)");
+        $statement = $connection->prepare("INSERT INTO movies(title, description,language, releast_date, duration,picture) VALUES (:title, :description,:language,:releast_date,:duration,:picture)");
         $statement->execute([
             ':title' => $title,
             ':description' => $description,
@@ -13,6 +13,7 @@
             ':picture' => $picture
 
         ]);
-
-        return $statement->rowCount() > 0;
     }
+
+    
+
