51bd52b7e6bae713ca1ff5e43420388ed823a69d
SOK NOEURN <soknoeurn-srun>
2023-02-10 07:46:51 +0700

    [PDN-75] define id of card

--- views/seller/seller.create.view.php
+++ views/seller/seller.create.view.php
@@ -1,7 +1,6 @@
 <?php 
-
 require "../partials/head.php";
-require "../../models/movie.create.model.php";
+require "../../movie.create.model.php";
 ?>
 
 <div class="mx-96">
@@ -37,7 +36,7 @@ require "../../models/movie.create.model.php";
                     <div class=" absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                         <svg aria-hidden="false" class=" w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
                     </div>
-                    <input datepicker type="text" class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date" id="date" name="release date">
+                    <input datepicker type="text" class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date" id="date" name="releastDate">
                 </div>
             </div>
 
@@ -48,12 +47,12 @@ require "../../models/movie.create.model.php";
             
             <div class="mb-4">
                 <label class="block text-red-700" for="image">Start Date</label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Input Movie Start Date" name="image">
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Input Movie Start Date" name="startDate">
             </div>
 
             <div class="mb-4">
                 <label class="block text-red-700" for="image">End Date</label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Input Movie End Date" name="image">
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Input Movie End Date" name="endDate">
             </div>
 
             <div class="mb-4">
@@ -78,9 +77,11 @@ require "../../models/movie.create.model.php";
                 <button class="ront-bold bg-gradient-to-r from-black via-gray-600 to-black hover:bg-slate-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" name="cancel">
                     Cancel
                 </button>
-                <button class="ront-bold bg-gradient-to-r from-black via-red-600 to-black hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" name="create">
+                <a href="/create"><button class="ront-bold bg-gradient-to-r from-black via-red-600 to-black hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" name="create">
                     Create
-                </button>           
+                </button> 
+                </a>
+                          
             </div>
         </form>
         
