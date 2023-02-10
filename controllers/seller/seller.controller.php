9112b5be6dbaae84166975ef2f92c4b1c9373772
dariyathorn <dariya.thorn@student.passerellesnumeriques.org>
2023-02-10 08:43:25 +0700

    [PDN-82] can delete data from table in databse

--- controllers/seller/seller.controller.php
+++ controllers/seller/seller.controller.php
@@ -1,8 +1,11 @@
 <?php
 require "models/seller.model.php";
+require "models/delete.model.php";
 // $heading = "Seller Page";
 
 if (isset($_GET['id'])){
-    echo 'hello';
+    $id = $_GET['id'];
+    deleteMovie($id);
+    header("location:/seller");
 }
 require "views/seller/seller.view.php";?>
