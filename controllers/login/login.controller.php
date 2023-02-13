0198f652733cc6e9287f49d67034592242cfb1ea
Phearom Chea <phearomchea210920@gmail.com>
2023-02-13 14:29:35 +0700

    [PDN-60] change seller views

--- controllers/login/login.controller.php
+++ controllers/login/login.controller.php
@@ -2,46 +2,8 @@
 session_start();
 require 'database/database.php';
 require 'models/users.model.php';
-    // variable get post
-    // if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    //     // variable get post
-    //     $email = $_POST['email'];
-    //     $password = $_POST['password'];
-    //     if(empty($password)) {
-    //         $errors['password'] = 'Please enter a password';
-    //     }
-    //     if(empty($email)) {
-    //         $errors['email'] = 'Please enter a email';
-    //     }
-    //     if(!filter_var($email, FILTER_VALIDATE_EMAIL)){  
-    //         $errors['email'] = "Invalid email format";  
-    //     }
-    //     if (empty($errors)){
-    //         $users = getUser();
-    //         foreach($users as $user){
-    //             $is_password = password_verify($password, $user['password']);
-    //             if ($email == $user["email"] && ($is_password || $password == $user['password'])){
-    //                 $_SESSION['email'] = $user["email"];
-    //                 $_SESSION['password'] = $user["password"];
-                    
-    //                 if($user["user_type"] == 'admin'|| $user["user_type"] == 'user'){
-    //                     $_SESSION['user_type'] = $user["user_type"];
-    //                 }else{
-    //                     $_SESSION['user_type'] = "";
-    //                 }
-    //                 // header('Location:/');
-    //                 if ($user["user_type"] == 'admin' || $user["user_type"] == 'user'){
-    //                     header('location:/');
-    //                 }else{
-    //                     header('location:/seller');
-    //                 }
-    //             }
-    //             else if(!($is_password)){
-    //                 $errors['password'] = 'Your password is not correct';
-    //             }
-    //         }
-    //     }
-    // }
+
+
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         // variable get post
         $email = $_POST['email'];
