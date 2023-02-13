0c28a757bae49315f3d4f3d1d38940164fa16963
Sreypok-D <sreypokdoem@gmail.com>
2023-02-13 14:50:13 +0700

    [PDN-72] insert data from input form create card of seller to movies and shows table

--- controllers/seller/seller.create.controller.php
+++ controllers/seller/seller.create.controller.php
@@ -22,28 +22,15 @@ else {
     if (!empty($_POST['title']) && !empty($_POST['description']) && !empty($_POST['language']) && !empty($_POST['releastDate']) && !empty($_POST['duration']) && !empty($_POST['picture'])){
         
         createMovie($title , $description , $language , $releastDate, $duration ,$picture);
-        // header("Location: /seller");
+        header("Location: /seller");
     }
 }
 
-
-
-// if($_SERVER['REQUEST_METHOD'] == 'POST') 
-// {
-//     $title = $_POST['title'];
-//     $description = $_POST['description'];
-//         $language = $_POST['language'];
-//         $releastDate = $_POST['releastDate'];
-//         $duration = $_POST['duration'];
-//         $picture = $_POST['picture'];
-//         // $start_date = $_POST['start_date'];
-//         // $end_date = $_POST['end_date'];
-//         // $format = $_POST['format'];
-//         // $subtitle = $_POST['subtitle'];
-//         echo $title ."<br>",$description."<br>" ,$language ."<br>"  , $releastDate ."<br>", $duration."<br>" ,$picture;
-//         if (!empty($_POST['title']) && !empty($_POST['description']) && !empty($_POST['language']) && !empty($_POST['releastDate']) && !empty($_POST['duration']) && !empty($_POST['picture'])){
-//             require("models/seller/seller.model.php"); 
-//             createMovie($title , $description , $language , $releastDate, $duration ,$picture);
-//             // header("Location: /seller");
-//         }
-// }
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (!empty($title) && !empty($description) && !empty($language) && !empty($releastDate) && !empty($duration) && !empty($picture) && !empty($startDate) && !empty($endDate) && !empty($format) && !empty($subtitle)){
+        $movieId =$cinemaId=createMovie($title , $description , $language , $releastDate, $duration ,$picture);
+        getToShow($startDate,$endDate,$format,$subtitle,$movieId,$cinemaId);
+        header("Location: /seller");
+    }
+     
+}
