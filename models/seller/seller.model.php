8177f4584e479255d5332876cac6521602ff0c6f
SOK NOEURN <soknoeurn-srun>
2023-02-15 20:46:06 +0700

    [PDN-77] change variable name on query modify

--- models/seller/seller.model.php
+++ models/seller/seller.model.php
@@ -18,12 +18,12 @@
     function updateMovie(string $title, string $description, string $language , string $releastDate, string $duration , string $picture, int $movie_id)
     {
         global $connection;
-        $statement = $connection->prepare("UPDATE movies SET title=:title, description=:description,language=:language, releast_date=:releast_date, duration=:duration,picture=:picture WHERE movie_id=:movie_id");
+        $statement = $connection->prepare("UPDATE movies SET title=:title, description=:description,language=:language, release_date=:release_date, duration=:duration,picture=:picture WHERE movie_id=:movie_id");
         $statement->execute([
             ':title' => $title,
             ':description' => $description,
             'language' => $language,
-            ':releast_date' => $releastDate,
+            ':release_date' => $releastDate,
             ':duration' => $duration,
             ':picture' => $picture,
             ':movie_id' => $movie_id
